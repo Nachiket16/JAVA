5dffeb4115f42d5e3cdacc55bd09f31da28d420b
Nachiket <nachiketkhule.nk@gmail.com>
2022-07-19 23:37:21 +0530

    Binary search using naive method

--- Searching & Sorting/searching.java
+++ Searching & Sorting/searching.java
@@ -7,8 +7,45 @@ class searchMain{
                 return i;
             }
         }
-
         return -1;
+    }
+
+    public int linearSearchRec(int[] arr, int key){
+        
+        int count = 0 ;
+        if(count == arr.length){
+            return -1;
+        }
+        if(arr[count] == key){
+            return count;
+        }else{
+            ++count;
+            return linearSearch(arr, key);
+        }
+    }
+
+    public int binarySearch(int[]arr, int key){
+
+        int start = 0 ;
+        int end = arr.length-1;
+
+        while(start <= end){
+
+            int mid = (start + end)/2;
+
+            if (arr[mid]==key) {    
+                return mid;
+            }
+            
+            if(key < arr[mid]){
+                end = mid-1; 
+            }else{
+                start = mid +1;
+            }
+
+        }
+        return -1;
+        
     }
 
 }
@@ -21,7 +58,9 @@ class main{
 
         searchMain obj = new searchMain();
         
-        System.out.println(obj.linearSearch(arr, 50));
+        System.out.println("Position of the Given key is: "+obj.linearSearch(arr, 50));
+        System.out.println("Position of the Given key is: "+obj.linearSearchRec(arr, 30));
+        System.out.println("Position of the Given key is: "+obj.binarySearch(arr, 10));
 
     }
 }
