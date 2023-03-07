a3fd25195e5eac5a51f5011ab198c67c6f1c794c
nachiket.khule <nachiketkhule.nk@gmail.com>
2023-03-07 23:25:40 +0530

    practice

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+T
+$CoreJava/src/Basics/commandLine.java,0/e/0e820a023c86c45b4f9271ceb5352ef13a96c1aa
+C
+Array/src/Main.java,d/a/da0d7d80b075937c8e0dcc5b03c2e7098ec5fbf4
---
+++ Array/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Main {
+
+  public static void main(String[] args) {
+    //2D array are like a matrix
+    /*
+      1 2 3
+      4 5 6
+      7 8 9
+    * */
+
+//    int [][] arr = new int[3][3];
+    //[row][col]
+    int [][] arr = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+
+
+  }
+}
